e6d5e765a17ce953d87358c8e0717c94344587fb
Seungjun Oh <osjuny11@gmail.com>
2023-05-07 18:37:15 +0900

    add sample location map

--- interaction_app.py
+++ interaction_app.py
@@ -8,7 +8,6 @@ external_stylesheets = [dbc.themes.BOOTSTRAP]
 app = Dash(__name__, external_stylesheets=external_stylesheets)
 
 
-# TODO: Interactive radio button
 # TODO: Weekly routine
 # TODO: Location
 
@@ -21,22 +20,45 @@ class Routine(Enum):
     EXERCISE = "Exercise Time"
 
 
+location_df = pd.read_csv("csv/sample_location.csv")
+px.set_mapbox_access_token(open(".mapbox_token").read())
+
+fig = px.scatter_mapbox(
+    location_df,
+    lat="latitude",
+    lon="longitude",
+    hover_name="timestamp",
+    hover_data=["latitude", "longitude"],
+    zoom=3,
+    width=500,
+    height=500
+)
+fig.update_layout(mapbox_style="streets")
+fig.update_layout(mapbox_bounds={"west": 127.35, "east": 127.37, "south": 36.36, "north": 36.38})
+
 # TODO: Styling components as option labels:
 # https://dash.plotly.com/dash-core-components/radioitems#styling-components-as-option-labels
 app.layout = html.Div(
-    dcc.RadioItems(
-        options=[
-            {"label": Routine.SLEEP.value, "value": Routine.SLEEP.name},
-            {"label": Routine.CLASS.value, "value": Routine.CLASS.name},
-            {"label": Routine.MEAL.value, "value": Routine.MEAL.name},
-            {"label": Routine.STUDY.value, "value": Routine.STUDY.name},
-            {"label": Routine.EXERCISE.value, "value": Routine.EXERCISE.name},
-        ],
-        value=Routine.SLEEP.name,
-        inline=True,
-        clearable=False,
-    )
+    [
+        html.Div(
+            dcc.RadioItems(
+                options=[
+                    {"label": Routine.SLEEP.value, "value": Routine.SLEEP.name},
+                    {"label": Routine.CLASS.value, "value": Routine.CLASS.name},
+                    {"label": Routine.MEAL.value, "value": Routine.MEAL.name},
+                    {"label": Routine.STUDY.value, "value": Routine.STUDY.name},
+                    {"label": Routine.EXERCISE.value, "value": Routine.EXERCISE.name},
+                ],
+                value=Routine.SLEEP.name,
+                inline=True,
+            )
+        ),
+        html.Div(
+            dcc.Graph(id="geographical-scatter", figure=fig)
+        ),
+    ]
 )
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)

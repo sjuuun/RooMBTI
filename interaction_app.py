aee2fa3a4e3842824fe75560527fea336ea8a050
Seungjun Oh <osjuny11@gmail.com>
2023-05-08 23:34:43 +0900

    weekly routine update

--- interaction_app.py
+++ interaction_app.py
@@ -22,23 +22,30 @@ class Routine(Enum):
 
 def weekly_routine_timeline(df: pd.DataFrame) -> go.Figure:
     fig = make_subplots(rows=7, cols=1, shared_xaxes=True, vertical_spacing=0.02)
-    fig_mon = px.timeline(df[df["weekday"] == 0], x_start="start", x_end="end", y="user_id")
-    fig_tue = px.timeline(df[df["weekday"] == 1], x_start="start", x_end="end", y="user_id")
-    fig_wed = px.timeline(df[df["weekday"] == 2], x_start="start", x_end="end", y="user_id")
-    fig_thr = px.timeline(df[df["weekday"] == 3], x_start="start", x_end="end", y="user_id")
-    fig_fri = px.timeline(df[df["weekday"] == 4], x_start="start", x_end="end", y="user_id")
-    fig_sat = px.timeline(df[df["weekday"] == 5], x_start="start", x_end="end", y="user_id")
-    fig_sun = px.timeline(df[df["weekday"] == 6], x_start="start", x_end="end", y="user_id")
+    timeline_figs = []
+    for i in range(7):
+        timeline_figs.append(
+            px.timeline(df[df["weekday"] == i], x_start="start", x_end="end", y="user_id", color="user_id")
+        )
 
-    fig.add_trace(go.Bar(fig_mon.data[0]), row=1, col=1)
-    fig.add_trace(go.Bar(fig_tue.data[0]), row=2, col=1)
-    fig.add_trace(go.Bar(fig_wed.data[0]), row=3, col=1)
-    fig.add_trace(go.Bar(fig_thr.data[0]), row=4, col=1)
-    fig.add_trace(go.Bar(fig_fri.data[0]), row=5, col=1)
-    fig.add_trace(go.Bar(fig_sat.data[0]), row=6, col=1)
-    fig.add_trace(go.Bar(fig_sun.data[0]), row=7, col=1)
+    for i in range(7):
+        f = timeline_figs[i]
+        show_legend = True if i == 0 else False
+        for j in range(len(f.data)):
+            fig.add_trace(go.Bar(f.data[j], showlegend=show_legend), row=(i+1), col=1)
 
-    fig.update_xaxes(type='date')
+    fig.update_yaxes(title="Monday", row=1, col=1)
+    fig.update_yaxes(title="Tuesday", row=2, col=1)
+    fig.update_yaxes(title="Wednesday", row=3, col=1)
+    fig.update_yaxes(title="Thursday", row=4, col=1)
+    fig.update_yaxes(title="Friday", row=5, col=1)
+    fig.update_yaxes(title="Saturday", row=6, col=1)
+    fig.update_yaxes(title="Sunday", row=7, col=1)
+
+    fig.update_xaxes(tickformat="%H:%M")
+    fig.update_layout(title="Weekly Routine")
+
+    fig.update_xaxes(type="date")
 
     return fig
 
@@ -72,8 +79,13 @@ for i in range(7):
         dict(type="SLEEP", user_id="Me", weekday=i, start="1970-01-01 23:10:00", end="1970-01-01 23:59:59"),
         dict(type="CLASS", user_id="Me", weekday=i, start="1970-01-01 10:07:00", end="1970-01-01 11:42:00"),
         dict(type="MEAL", user_id="Me", weekday=i, start="1970-01-01 12:20:00", end="1970-01-01 13:07:00"),
+        dict(type="STUDY", user_id="Me", weekday=i, start="1970-01-01 19:20:00", end="1970-01-01 21:07:00"),
+        dict(type="EXERCISE", user_id="Me", weekday=i, start="1970-01-01 21:30:00", end="1970-01-01 22:47:00"),
         dict(type="SLEEP", user_id="Roommate", weekday=i, start="1970-01-01 02:00:00", end="1970-01-01 11:42:00"),
         dict(type="CLASS", user_id="Roommate", weekday=i, start="1970-01-01 12:55:00", end="1970-01-01 15:02:00"),
+        dict(type="MEAL", user_id="Roommate", weekday=i, start="1970-01-01 17:20:00", end="1970-01-01 18:07:00"),
+        dict(type="STUDY", user_id="Roommate", weekday=i, start="1970-01-01 20:20:00", end="1970-01-01 21:35:00"),
+        dict(type="EXERCISE", user_id="Roommate", weekday=i, start="1970-01-01 16:01:00", end="1970-01-01 16:54:00"),
     ])
     weekly_df = pd.concat([weekly_df, _df])
 

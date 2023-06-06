a36b876acdc572613cca3b92b61de91acf9ba9a3
Seungjun Oh <osjuny11@gmail.com>
2023-06-07 01:15:48 +0900

    BFI → Personal traits 로 제목 변경

--- figures/bfi.py
+++ figures/bfi.py
@@ -16,7 +16,7 @@ def bfi_single(user_id: str) -> go.Figure:
             name="You"
         )
     )
-    bfi_fig.update_layout(template="simple_white", title="BFI", showlegend=True)
+    bfi_fig.update_layout(template="simple_white", title="Personal traits", showlegend=True)
     bfi_fig.update_polars(radialaxis_angle = 90, radialaxis_range=[0, 15], radialaxis_tickangle=90, angularaxis_rotation=90, angularaxis_direction = "clockwise")
     return bfi_fig
 
@@ -42,7 +42,7 @@ def bfi_compare(me_id: str, roommate_id: str) -> go.Figure:
             name="Roommate"
         )
     )
-    bfi_fig.update_layout(template="simple_white", title="BFI")
+    bfi_fig.update_layout(template="simple_white", title="Personal traits")
     bfi_fig.update_polars(radialaxis_angle = 90, radialaxis_range=[0, 15], radialaxis_tickangle=90, angularaxis_rotation=90, angularaxis_direction = "clockwise")
     return bfi_fig
 

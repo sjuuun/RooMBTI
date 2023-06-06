cd0a21c3f6a1df818455841d093a02563c3590fe
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2023-06-06 22:03:30 +0900

    주석 처리된 legacy 코드 삭제 (#50)

--- figures/similarity_barpolar.py
+++ figures/similarity_barpolar.py
@@ -1,22 +1,6 @@
 import plotly.graph_objs as go
 
 def half_ring_plot(percentage):
-    # fig = go.FigureWidget()
-    # fig.add_barpolar(
-    #     r=[1 for _ in range(100)],
-    #     theta=[180 - i for i in range(180)],
-    #     offset=0,
-    #     marker={'color': [1 if i <= percentage*1.8 else 2 for i in range(180)]}
-    # )
-    # fig.layout.polar.radialaxis.title = dict(text="Similarity")
-    # fig.layout.polar.hole = 0.8
-    # fig.layout.polar.angularaxis.showgrid = False
-    # fig.layout.polar.radialaxis.showgrid = False
-    # fig.layout.polar.radialaxis.range = [0, 1]
-    # fig.layout.polar.radialaxis.tickvals = []
-    # fig.layout.polar.bargap = 0.9
-    # fig.layout.polar.sector = [0, 180]
-    # fig.layout.polar.domain.x = [0, 1]
     fig = go.Figure(go.Indicator(
     mode = "gauge+number",
     value = percentage,
